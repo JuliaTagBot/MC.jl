8f862808f0f00000ee3b5ab4774d9a988353d9b1
Carl Morris <carl.morris.world@outlook.com>
2019-11-30 09:44:42 +0800

    Add basic fixed length sampling of arrays.
    Add ParallelSample struct and overloads of sample_rvs to support
    sampling a fixed number of elements from each expression RV and
    iterating over them using zip.

--- src/MC.jl
+++ src/MC.jl
@@ -1,9 +1,10 @@
 module MC
 
-export RV, ArrayRV, sample
+export RV, ArrayRV, sample, hist, ProductSample, ParallelSample
 
 import Base
 using Base.Iterators: product
+using DataStructures: counter
 
 # Random Variables
 abstract type RV end
@@ -16,7 +17,7 @@ sample(rv::ArrayRV) = rv.variates
 
 # Expressions of Random Variables
 struct NExpr{N}
-    rvs::NTuple{N, RV}
+    rvs::NTuple{N,RV}
     f
 end
 
@@ -25,16 +26,29 @@ apply(f, ex::NExpr) = apply(f ∘ ex.f, ex.rvs)
 Base.in(rv::RV, ex::NExpr) = any(rv === exrv for exrv in ex.rvs)
 
 struct ProductSample end
+struct ParallelSample
+    n
+end
 
 function sample_rvs(ex::NExpr, ::ProductSample)
     variates = map(sample, ex.rvs)
     @debug variates
     n_out = *(length.(variates)...)
     @debug n_out
-    reshape(collect(product(variates...)), n_out), n_out
+    # reshape(collect(product(variates...)), n_out), n_out
+    product(variates...), n_out
 end
 
-function sample(ex::NExpr{N}, method=ProductSample()) where N
+function sample_rvs(ex::NExpr, method::ParallelSample)
+    variates = map(x -> rand(sample(x), method.n), ex.rvs)
+    @debug variates
+    n_out = method.n
+    @debug n_out
+    # reshape(collect(product(variates...)), n_out), n_out
+    zip(variates...), n_out
+end
+
+function sample(ex::NExpr{N}, method = ProductSample()) where {N}
     variates, n_out = sample_rvs(ex, method)
     sample(ex.f, variates, n_out)
 end
@@ -58,7 +72,7 @@ function find(rv::RV, ex::NExpr)
     return nothing
 end
 
-function merge_indexes(ex1::NExpr{N1}, ex2::NExpr{N2}) where {N1, N2}
+function merge_indexes(ex1::NExpr{N1}, ex2::NExpr{N2}) where {N1,N2}
     idx = zeros(Int, N2)
     k = 1
     for j in eachindex(ex2.rvs)
@@ -73,39 +87,46 @@ function merge_indexes(ex1::NExpr{N1}, ex2::NExpr{N2}) where {N1, N2}
     idx
 end
 
-function apply(f, ex1::NExpr{N1}, ex2::NExpr{N2}) where {N1, N2}
+function apply(f, ex1::NExpr{N1}, ex2::NExpr{N2}) where {N1,N2}
     rightkept = [rv for rv in ex2.rvs if rv ∉ ex1]
     rightapply = merge_indexes(ex1, ex2)
     unique_rvs = tuple(ex1.rvs..., rightkept...)
     NExpr(
-    unique_rvs,
-    (x...) -> f(
-        ex1.f([x[i] for i in eachindex(ex1.rvs)]...),
-        ex2.f([x[i] for i in rightapply]...)))
+        unique_rvs,
+        (x...) -> f(
+            ex1.f([x[i] for i in eachindex(ex1.rvs)]...),
+            ex2.f([x[i] for i in rightapply]...),
+        ),
+    )
 end
 
 Base.convert(::Type{<:NExpr}, x::RV) = apply(x -> x, x)
-Base.promote_rule(::Type{<:NExpr{N}}, ::Type{<:RV}) where N = NExpr{N}
+Base.promote_rule(::Type{<:NExpr{N}}, ::Type{<:RV}) where {N} = NExpr{N}
 
 # Define basic unary operators.
-for op in (:+, :-, :!)
-    @eval Base.$op(x::Union{NExpr, RV}) = apply(x -> $op(x), x)
+const UNARY_OP = (:+, :-, :!)
+for op in UNARY_OP
+    @eval Base.$op(x::Union{NExpr,RV}) = apply(x -> $op(x), x)
 end
 
 # Define basic binary operators.
-for op in (:+, :-, :/, :*)
-    @eval function Base.$op(x::Union{NExpr, RV}, y::Union{NExpr, RV})
+const BINARY_OP = (:+, :-, :/, :*)
+for op in BINARY_OP
+    @eval function Base.$op(x::Union{NExpr,RV}, y::Union{NExpr,RV})
         apply((x, y) -> $op(x, y), promote(x, y)...)
     end
-    @eval function Base.$op(x::Union{NExpr, RV}, y)
+    @eval function Base.$op(x::Union{NExpr,RV}, y)
         apply(x -> $op(x, y), x)
     end
-    @eval function Base.$op(x, y::Union{NExpr, RV})
+    @eval function Base.$op(x, y::Union{NExpr,RV})
         apply(y -> $op(x, y), y)
     end
 end
 
 # Sample for numbers so that sample can be called on RV or number.
 sample(x::Number) = [x]
+
+# Histogram for comparing samples.
+hist(x) = sort(Dict(counter(x)))
 
 end # module
